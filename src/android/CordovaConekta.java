01c32481e51c92ebc493d1b54873215e27a10fa0
nwrman <nwrman@gmail.com>
2019-11-09 01:56:29 -0600

    Set Up Conekta

--- src/android/CordovaConekta.java
+++ src/android/CordovaConekta.java
@@ -1,32 +1,81 @@
-package cordova-plugin-conekta;
+package com.nwrman.cordova.conekta;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import io.conekta.conektasdk.Conekta;
+import io.conekta.conektasdk.Card;
+import io.conekta.conektasdk.Token;
+import android.util.Log;
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class CordovaConekta extends CordovaPlugin {
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
-            return true;
-        }
-        return false;
+    private Token conektaTokenInstance;
+    private static final String TAG = "Conekta"
+
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        Log.d(TAG, "Init");
+        Conekta.collectDevice(cordova.getActivity());
+        conektaTokenInstance = new Token(cordova.getActivity());
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
+    @Override
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("setPublicKey")) {
+            Log.d(TAG, "setKey");
+            Log.d(TAG, data.getString(0));
+            setPublicKey(data.getString(0), callbackContext);
+        } else if (action.equals("createCardToken")) {
+            Log.d(TAG, "orderCreateToken");
+            createCardToken(data.getJSONObject(0), callbackContext);
         } else {
-            callbackContext.error("Expected one non-empty string argument.");
+            return false;
         }
+
+        return true;
+    }
+
+    private void setPublicKey(final String key, final CallbackContext callbackContext) {
+        Log.d(TAG, "actual set key");
+        try {
+            Conekta.setPublicKey(key);
+            Log.d(TAG, "set key success");
+            callbackContext.success();
+        } catch (Exception e) {
+            Log.d(TAG, "set key failed");
+            callbackContext.error(e.getLocalizedMessage());
+        }
+    }
+
+    private void createCardToken(final JSONObject creditCard, final CallbackContext callbackContext) {
+
+        Log.d(TAG, "actual createToken");
+
+        Card card = new Card("Josue Camara", "4242424242424242", "332", "11", "2019");
+
+        conektaTokenInstance.onCreateTokenListener(new Token.CreateToken() {
+            @Override
+            public void onCreateTokenReady(JSONObject data) {
+                try {
+                    callbackContext.success(data.getString("id"));
+                    Log.d(TAG, 'Token got: ' + data.getString("id"));
+                } catch (Exception err) {
+                    callbackContext.error("Error: " + err.toString());
+                    Log.d(TAG, "Error: " + err.toString());
+                }
+            }
+        });
+
+        conektaTokenInstance.create(card);//Create token
     }
 }
--- www/CordovaConekta.js
+++ www/CordovaConekta.js
@@ -1,5 +1,54 @@
 var exec = require('cordova/exec');
+var noop = function(){};
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'CordovaConekta', 'coolMethod', [arg0]);
+/**
+ * @namespace cordova.plugins
+ */
+
+/**
+ * Parameters to create a credit card token
+ * @typedef module:stripe.CreditCardTokenParams
+ * @type {Object}
+ * @property {string} number Card number
+ * @property {number} expMonth Expiry month
+ * @property {number} expYear Expiry year
+ * @property {string} [cvc] CVC/CVV
+ * @property {string} [name] Cardholder name
+ * @property {string} [address_line1] Address line 1
+ * @property {string} [address_line2] Address line 2
+ * @property {string} [address_city] Address line 2
+ * @property {string} [address_state] State/Province
+ * @property {string} [address_country] Country
+ * @property {string} [postalCode] Postal/Zip code
+ * @property {string} [currency] 3-letter code for currency
+ */
+
+/**
+ * @exports conekta
+ */
+module.exports = {
+
+  /**
+   * Set publishable key
+   * @param key {string} Publishable key
+   * @param [success] {Function} Success callback
+   * @param [error] {Function} Error callback
+   */
+  setPublicKey: function(key, success, error) {
+    success = success || noop;
+    error = error || noop;
+    exec(success, error, "CordovaConekta", "setPublicKey", [key]);
+  },
+
+  /**
+   * Create a credit card token
+   * @param creditCard {module:stripe.CreditCardTokenParams} Credit card information
+   * @param success {Function} Success callback
+   * @param error {Function} Error callback
+   */
+  createCardToken: function(creditCard, success, error) {
+    success = success || noop;
+    error = error || noop;
+    exec(success, error, "CordovaConekta", "createCardToken", [creditCard]);
+  },
 };
