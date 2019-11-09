0d2df1ed3a19759497c66ebc7a1b6f18c1940a66
nwrman <nwrman@gmail.com>
2019-11-08 22:51:45 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Cordova Conekta Plugin
+A Cordova plugin that lets you use Conekta's Native SDKs for Android, iOS and Browser.
+
+## Installation
+```shell
+cordova plugin add cordova-plugin-conekta
+```
+
+<br>
+
+## Usage
+
+First we need to set our publishable key. This can be your test or live key.
+```javascript
+cordova.plugins.conekta.setPublishableKey('pk_test_MyPublishableKey');
+```
---
+++ src/ios/CordovaConekta.m
@@ -0,0 +1,28 @@
+/********* CordovaConekta.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface CordovaConekta : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation CordovaConekta
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
