f3884c12761d139691e612004704489418bb8d75
nwrman <nwrman@gmail.com>
2019-11-10 12:39:49 -0600

    Get token with card info provided from caller

--- src/android/CordovaConekta.java
+++ src/android/CordovaConekta.java
@@ -12,31 +12,24 @@ import org.json.JSONObject;
 import io.conekta.conektasdk.Conekta;
 import io.conekta.conektasdk.Card;
 import io.conekta.conektasdk.Token;
+
 import android.util.Log;
 
-/**
- * This class echoes a string called from JavaScript.
- */
 public class CordovaConekta extends CordovaPlugin {
 
     private Token conektaTokenInstance;
-    private static final String TAG = "Conekta"
+    private static final String TAG = "Conekta";
 
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
-        Log.d(TAG, "Init");
-        Conekta.collectDevice(cordova.getActivity());
         conektaTokenInstance = new Token(cordova.getActivity());
     }
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
         if (action.equals("setPublicKey")) {
-            Log.d(TAG, "setKey");
-            Log.d(TAG, data.getString(0));
             setPublicKey(data.getString(0), callbackContext);
         } else if (action.equals("createCardToken")) {
-            Log.d(TAG, "orderCreateToken");
             createCardToken(data.getJSONObject(0), callbackContext);
         } else {
             return false;
@@ -46,10 +39,8 @@ public class CordovaConekta extends CordovaPlugin {
     }
 
     private void setPublicKey(final String key, final CallbackContext callbackContext) {
-        Log.d(TAG, "actual set key");
         try {
             Conekta.setPublicKey(key);
-            Log.d(TAG, "set key success");
             callbackContext.success();
         } catch (Exception e) {
             Log.d(TAG, "set key failed");
@@ -59,23 +50,37 @@ public class CordovaConekta extends CordovaPlugin {
 
     private void createCardToken(final JSONObject creditCard, final CallbackContext callbackContext) {
 
-        Log.d(TAG, "actual createToken");
+        Card card = null;
 
-        Card card = new Card("Josue Camara", "4242424242424242", "332", "11", "2019");
+        try {
+            card = new Card(
+                    creditCard.getString("name"),
+                    creditCard.getString("number"),
+                    creditCard.getString("cvc"),
+                    creditCard.getString("exp_month"),
+                    creditCard.getString("exp_year")
+            );
+        } catch (JSONException e) {
+            callbackContext.error("Error: Todos los datos de la tarjeta son requeridos");
+        }
 
         conektaTokenInstance.onCreateTokenListener(new Token.CreateToken() {
             @Override
             public void onCreateTokenReady(JSONObject data) {
                 try {
                     callbackContext.success(data.getString("id"));
-                    Log.d(TAG, 'Token got: ' + data.getString("id"));
                 } catch (Exception err) {
-                    callbackContext.error("Error: " + err.toString());
-                    Log.d(TAG, "Error: " + err.toString());
+                    try {
+                        callbackContext.error("Conekta - " + data.getString("message_to_purchaser"));
+                    } catch (JSONException e) {
+                        callbackContext.error("¡Error no especificado! 😱");
+                    }
                 }
             }
         });
 
-        conektaTokenInstance.create(card);//Create token
+        if (card != null) {
+            conektaTokenInstance.create(card);
+        }
     }
 }
